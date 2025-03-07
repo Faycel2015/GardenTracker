3582e93a46c3babfb35251b787e808ebf0029071
Faycel2015 <faycel.ayech@gmail.com>
2025-03-07 23:22:20 +0100

    Initial commit - Set up GardenTracker project

---
+++ GardenTracker/GardenTracker/GardenTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenTrackerApp.swift
+//  GardenTracker
+//
+//  Created by FayTek on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GardenTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
